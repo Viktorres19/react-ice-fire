b794a5d715553df2cd15df34c35dcceb1dc304ce
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-08-05 18:08:56 +0300

    100-patterns-react

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,34 +1,36 @@
 import React, {Component} from 'react';
-import gotService from './../../service/gotService';
 import './itemList.css';
 import Spinner from '../spinner';
 
 export default class ItemList extends Component {
-
-  gotService = new gotService();
 
   state = {
     charList: null
   }
 
   componentDidMount() {
-    this.gotService.getAllCharacters()
-      .then((charList) => {
+    const {getData} = this.props;
+
+    getData()
+      .then((itemList) => {
         this.setState({
-          charList
+          itemList
         })
       })
   }
 
   renderItems(arr) {
-    return arr.map((item, i) => {
+    return arr.map((item) => {
+      const {id} = item;
+      const label = this.props.renderItem(item)
+
       return (
         <li
-          key={i}
+          key={id}
           className="list-group-item"
-          onClick={ () => this.props.onCharSelected(41 + i)}
+          onClick={ () => this.props.onItemSelected(id)}
         >
-          {item.name}
+          {label}
         </li>
       )
     })
@@ -36,13 +38,13 @@ export default class ItemList extends Component {
 
   render() {
 
-    const {charList} = this.state;
+    const {itemList} = this.state;
 
-    if(!charList) {
+    if(!itemList) {
       return <Spinner />
     }
 
-    const items = this.renderItems(charList);
+    const items = this.renderItems(itemList);
 
     return (
       <ul className="item-list list-group">
